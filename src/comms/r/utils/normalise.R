e0446b1d8ae0bcde9089144bfab2b02d0e03a676
Sam Holland <hol_sam@icloud.com>
2026-05-08 12:11:24 +0100

    fix(normalise): update normalise utility script
    - Modified r/utils/normalise.R to use dNSAF instead of original NSAF.

--- src/comms/r/utils/normalise.R
+++ src/comms/r/utils/normalise.R
@@ -2,7 +2,7 @@
 # normalise.R: normalisation utilities
 
 # logNSAF: applies log(x + epsilon) transform to a numeric matrix or tibble column; epsilon defaults to half the minimum non-zero value across the input
-logNSAF <- function(x, epsilon=NULL) {
+logdNSAF <- function(x, epsilon=NULL) {
   if (is.null(epsilon)) {
     nonzero <- x[x > 0 & !is.na(x)]
     epsilon <- if (length(nonzero) > 0) min(nonzero) / 2 else 1e-10
@@ -11,19 +11,19 @@ logNSAF <- function(x, epsilon=NULL) {
 }
 
 # medianShiftNormalise: applies within-fraction median-shift normalisation to a wide NSAF tibble
-medianShiftNormalise <- function(nsafWide, sampleMetadata) {
+medianShiftNormalise <- function(dnsafWide, sampleMetadata) {
   fractions <- unique(sampleMetadata$fraction)
   for (frac in fractions) {
     frac_cols <- sampleMetadata %>%
       filter(fraction == frac) %>%
-      pull(nsaf_col)
-    frac_cols <- intersect(frac_cols, colnames(nsafWide))
+      pull(dnsaf_col)
+    frac_cols <- intersect(frac_cols, colnames(dnsafWide))
     if (length(frac_cols) < 2) next
-    global_median <- median(unlist(nsafWide[, frac_cols]), na.rm=TRUE)
+    global_median <- median(unlist(dnsafWide[, frac_cols]), na.rm=TRUE)
     for (col in frac_cols) {
-      col_median <- median(nsafWide[[col]], na.rm=TRUE)
-      nsafWide[[col]] <- nsafWide[[col]] - col_median + global_median
+      col_median <- median(dnsafWide[[col]], na.rm=TRUE)
+      dnsafWide[[col]] <- dnsafWide[[col]] - col_median + global_median
     }
   }
-  return(nsafWide)
+  return(dnsafWide)
 }
