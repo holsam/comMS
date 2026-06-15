31d65fbf3b81da6827526086eac07b8339348eb2
Sam Holland <hol_sam@icloud.com>
2026-06-15 15:38:39 +0100

    style(license): update license to use pager
    - Modified commands/license.py to use pydoc pager instead of printing
    license to terminal.

--- src/comms/commands/license.py
+++ src/comms/commands/license.py
@@ -3,12 +3,13 @@ comMS print license to terminal
 '''
 
 # -- Import external dependencies
-import typer
+import pydoc
 from importlib.resources import files as pkg_files
 from rich import print
 
 # -- printLicense: prints the comMS license to terminal and exits
 def printLicense():
     with pkg_files('comms').joinpath('../../LICENSE').open('r') as f:
-        print(f'\n{f.read()}')
+        pydoc.pager(f.read())
+        print(f'comMS is distributed under the [bold cyan]GPL-3.0 license[/bold cyan].\n')
     raise SystemExit(0)
